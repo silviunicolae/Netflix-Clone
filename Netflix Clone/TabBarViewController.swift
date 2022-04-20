3cb15d2740b14a2a28c475148282c742ea696e1a
Silviu <nicolae.silviu.c@gmail.com>
2022-04-20 18:01:09 +0300

    initial setup

---
+++ Netflix Clone/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Silviu Nicolae on 20.04.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
